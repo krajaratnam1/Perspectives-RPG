9bcd3c9280833cfa8861e32bf097d3bb499ccef2
willrhodes <wrhodes42@gmail.com>
2020-02-25 16:45:27 -0500

    character controller

---
+++ Perspectives Puzzle/Assets/Scripts/PlayerSwitch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitch : MonoBehaviour
+{
+    bool isPlayerA = true;
+
+    public void SwitchPlayers(){
+        isPlayerA = !isPlayerA;
+    }
+}
